311f44c0a4e553254ba213e1522d4b7ec0b58f40
fitrianlstr <fitrilstr02@gmail.com>
2023-10-16 11:17:55 +0700

    first commit

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+51
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Fitriani</li>
+ <li>Nama Belakang : Lestari</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan kesadaran III cip muara</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Cirebon</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Bulutangkis</li>
+ <li>Catur</li>
+ </ul>
+</section>
